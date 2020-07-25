8caadd1d0f7f258e8da1b62f73efc9870c8141de
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-07-25 20:43:12 +0900

    敵プレハブの作成

---
+++ Assets/Scripts/Game/EnemyManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _numText;
+
+    private const float SPEED = 2;
+    
+    private int num;
+    private Vector3 moveDirection;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        { 
+            Reflection(collision);
+        }
+       
+    }
+
+    public void SetNum(int num) {
+        this.num = num;
+        _numText.text = num.ToString();
+    }
+
+    public void Move(Vector3 direction)
+    {
+        moveDirection = direction.normalized;
+        GetComponent<Rigidbody>().velocity = direction.normalized * SPEED;
+    }
+
+    private void Reflection(Collision collision)
+    {
+        Vector3 normal = collision.contacts[0].normal;
+        Vector3 reflect = Vector3.Reflect(moveDirection, normal);
+        Debug.Log($"{reflect.x} {reflect.y} {reflect.z}");
+        Move(reflect);
+    }
+}
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -6,14 +6,32 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private CharacterManager _characterManager;
     [SerializeField] private Joystick _joystick;
+    [SerializeField] private GameObject _enemyPrefab;
 
     private void Start()
     {
         ConnectCharaAndJoystick();
+
+        GameStart();
     }
 
     private void ConnectCharaAndJoystick()
     {
         _characterManager.joystick = _joystick;
+    }
+
+    private void GameStart()
+    {
+        CreateEnemy();
+    }
+
+    private void CreateEnemy() {
+        var y = 1.2f;
+        var enemy = (GameObject)Instantiate(_enemyPrefab);
+        enemy.transform.position = new Vector3(0, y, 2.66f);
+
+        var enemyManager = enemy.GetComponent<EnemyManager>();
+        enemyManager.SetNum(3);
+        enemyManager.Move(new Vector3(1, 0, 1));
     }
 }

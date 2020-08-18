5107e661488fec147d7dc21f2c058de4a0fe449d
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-08-18 22:49:14 +0900

    負け画面のアニメーション

--- Assets/Scripts/UI/Window/Defeat/DefeatWindowUIScript.cs
+++ Assets/Scripts/UI/Window/Defeat/DefeatWindowUIScript.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
 using System;
+using DG.Tweening;
 
 public class DefeatWindowUIScript : WindowBase
 {
+    [SerializeField] private GameObject _countDownBase;
+    [SerializeField] private Image _countdownImage;
+    [SerializeField] private List<Sprite> _countdownSpriteList;
     [SerializeField] private Button _continueButton;
     [SerializeField] private Button _noThanksButton;
 
+    private const float COUNTDOWN_TIME = 5f; // カウントダウン時間
+    private const int MAX_COUNTDOWN_NUM = 3; // カウントダウンの最大値(3->2->1)
+
     private Action onClickContinue;
     private Action onClickNoThanks;
+    private IDisposable countdownAnimationObservable;
 
     public override void Init(Dictionary<string,object> param)
     {
@@ -21,6 +29,7 @@ public class DefeatWindowUIScript : WindowBase
         onClickNoThanks = (Action)param["onClickNoThanks"];
 
         _continueButton.OnClickAsObservable()
+            .Do(_ => Dispose())
             .SelectMany(_ => UIManager.Instance.CloseWindowObservable(gameObject))
             .Do(_ =>
             {
@@ -33,6 +42,7 @@ public class DefeatWindowUIScript : WindowBase
             .Subscribe();
 
         _noThanksButton.OnClickAsObservable()
+            .Do(_ => Dispose())
             .Do(_ =>
             {
                 if (onClickNoThanks != null)
@@ -42,5 +52,49 @@ public class DefeatWindowUIScript : WindowBase
                 }
             })
             .Subscribe();
+
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        countdownAnimationObservable = Observable.ReturnUnit()
+            .Do(_ =>
+            {
+                _countDownBase.SetActive(true);
+                _continueButton.gameObject.SetActive(true);
+                _noThanksButton.gameObject.SetActive(false);
+            })
+            .SelectMany(_ => PlayCountdownAnimation(3))
+            .SelectMany(_ => PlayCountdownAnimation(2))
+            .SelectMany(_ => PlayCountdownAnimation(1))
+            .Do(_ =>
+            {
+                _countDownBase.SetActive(false);
+                _continueButton.gameObject.SetActive(false);
+                _noThanksButton.gameObject.SetActive(true);
+            })
+            .Subscribe();
+    }
+
+    private IObservable<Unit> PlayCountdownAnimation(int num) {
+        var initialScale = 0.8f;
+
+        if (num <= 0) return Observable.ReturnUnit();
+
+        return DOTween.Sequence()
+            .SetUpdate(true)
+            .OnStart(() =>
+            {
+                _countdownImage.sprite = _countdownSpriteList[num - 1];
+                _countdownImage.transform.localScale = Vector3.one * initialScale;
+            })
+            .Append(_countdownImage.transform.DOScale(Vector3.one, COUNTDOWN_TIME / MAX_COUNTDOWN_NUM).SetUpdate(true))
+            .PlayAsObservable()
+            .AsUnitObservable();
+    }
+
+    private void Dispose() {
+        if (countdownAnimationObservable != null) countdownAnimationObservable.Dispose();
     }
 }
